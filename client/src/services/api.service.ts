f5f905444120fd6c62dccd1bced396367fa44cff
Narendra Samtani <alenway97@gmail.com>
2025-11-14 11:27:29 +0530

    set the backend render url in service client

--- client/src/services/api.service.ts
+++ client/src/services/api.service.ts
@@ -1,7 +1,8 @@
 // services/api.service.ts
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API_URL =
+    import.meta.env.VITE_API_URL || "https://api-selo-notesapp.onrender.com";
 
 const api = axios.create({
     baseURL: API_URL,
